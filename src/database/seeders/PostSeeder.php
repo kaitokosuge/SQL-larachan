c9922dab4ce1589b0e187bc5dda5d50595c24d5e
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-21 16:55:47 +0900

    add: post seeder

---
+++ src/database/seeders/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 11; $i++) {
+            DB::table("posts")->insert([
+                'title' => "hello world",
+                'content'  => "Just Do it.",
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
